51ad95496b43f64413590e20c712874aaf9a552b
Amit Bhadoria <gnulib@gmail.com>
2015-11-19 23:56:08 -0800

    Fixes #3 by adding BCrypt password encoder bean usage

--- src/main/java/com/integratingfactor/idp/lib/config/SecurityConfig.java
+++ src/main/java/com/integratingfactor/idp/lib/config/SecurityConfig.java
@@ -2,33 +2,56 @@ package com.integratingfactor.idp.lib.config;
 
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static Logger LOG = Logger.getLogger(SecurityConfig.class.getName());
+    
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    
+    private String defaultPassword;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         LOG.info("Using in-memory user details service with hard coded users: \"user\" and \"admin\"");
+        setDefaultPassword(passwordEncoder.encode("password"));
         auth
                 // enable in memory based authentication with a user named
                 // "user" and "admin"
-                .inMemoryAuthentication().withUser("user").password("password").roles("USER").and()
-                .withUser("admin").password("password").roles("USER", "ADMIN");
+                .inMemoryAuthentication().withUser("user").password(getDefaultPassword()).roles("USER").and()
+                .withUser("admin").password(getDefaultPassword()).roles("USER", "ADMIN").and()
+                .passwordEncoder(passwordEncoder);
     }
 
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    public String getDefaultPassword() {
+        return defaultPassword;
+    }
+
+    public void setDefaultPassword(String defaultPassword) {
+        this.defaultPassword = defaultPassword;
     }
 
     /**
